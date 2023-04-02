897ff4b1ba3bd2f6db3a545f0e18a1a5c46a291b
mattiolka <mattiolka@gmail.com>
2023-04-02 16:12:03 +0300

    Удосконалила роботу світофора

--- app.js
+++ app.js
@@ -11,26 +11,20 @@ const colors = [
     {color: green, on: '#2ECC40', off: '#0f2a12', boxShadow: '0 0 10px rgba(46, 204, 64, 0.5)'}
     ];
 
-let current = 'green';
-let previous = null;
 let isActive;
 
-buttonOff.addEventListener('click', trafficLightsOff);
-buttonOn.addEventListener('click', trafficLightsOn);
-
-function trafficLightsOff() {
+buttonOff.addEventListener('click', () => {
     offColors();
     isActive = false;
     buttonOff.style.backgroundColor = '#dd180e';
     buttonOn.style.backgroundColor = '#a8cea2';
-}
+});
 
-function trafficLightsOn() {
+buttonOn.addEventListener('click', () => {
     isActive = true;
     current = 'green';
     process();
-}
-
+});
 
 function offColors(){
     colors.forEach(i => {
@@ -40,69 +34,36 @@ function offColors(){
 }
 
 function redOn() {
-    offColors();
     colors[0].color.style.backgroundColor = colors[0].on;
     colors[0].color.style.boxShadow = colors[0].boxShadow;
 } 
 function yellowOn() {
-    offColors();
     colors[1].color.style.backgroundColor = colors[1].on;
     colors[1].color.style.boxShadow = colors[1].boxShadow;
 }
 function greenOn() {
-    offColors();
     colors[2].color.style.backgroundColor = colors[2].on;
     colors[2].color.style.boxShadow = colors[2].boxShadow;
 }
     
-function nextCurrent() {
-    if(current == 'green'){
-        previous = current;
-        current = 'yellow';
-        return;
-    }
-
-    if(current == 'red'){
-        previous = current;
-        current = 'yellow';
-        return;
-    }
-
-    if(current == 'yellow'){
-        let tempCurrent = current
-        current = previous == 'green' ? 'red' : 'green';
-        previous = tempCurrent;
-        return;
-    }
-}
-
 function process(){
     if (!isActive) return;
 
-    if(current == 'green'){
-        greenOn();
-        setTimeout(() => {
-            nextCurrent();
-            process();
-        }, 2000);
+    function nextCurrent() {
+        if(current == 'green') return [current, 'yellow']
+        if(current == 'red') return [curent, 'yellow']
+        if(current == 'yellow') return [current, previous == 'green' ? 'red' : 'green']
     }
+ 
+    offColors();
+    if(current === 'green') greenOn();
+    if(current === 'red') redOn();
+    if(current === 'yellow') yellowOn();
 
-    if(current == 'red'){
-        redOn();
-        setTimeout(() => {
-            nextCurrent();
-            process();
-        }, 2000);
-    }
-
-    if(current == 'yellow'){
-        yellowOn();
-        setTimeout(() => {
-            nextCurrent();
-            process();
-        }, 500);
-    }
-
+    setTimeout(() => {
+        [previous, current] =  nextCurrent();
+        process();
+    }, (current === 'green' || current === 'red') ? 1000 : 200 );
 
     buttonOn.style.backgroundColor = '#28df0b';
     buttonOff.style.backgroundColor = '#aa7875';
